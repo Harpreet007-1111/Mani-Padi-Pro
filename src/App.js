ba7fa683125344dcec0ed1d048a44b11556054e6
Harpreet007-1111 <jassyharpreet123@gmail.com>
2023-09-03 17:42:07 +0300

    "update"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Footer from './components/Footer';
+import Services from './components/Services';
+import Test from './components/Test';
+import Contacts from './components/Contacts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+
+        <section id="home">
+          <Home />
+        </section>
+
+        <section id='about'>
+          <About />
+        </section>
+
+        <section id='services'>
+          <Services />
+        </section>
+
+        <section id='test'>
+          <Test />
+        </section>
+
+        <section id='contact'>
+          <Contacts />
+        </section>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
